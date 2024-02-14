8b71db270aae7b8565704d1be48e2f7f1ebb2bd5
Brian Lee <bnlee419@gmail.com>
2024-02-13 17:26:06 -0800

    added express servers

--- littlethings/src/app/page.tsx
+++ littlethings/src/app/page.tsx
@@ -1,5 +1,16 @@
+'use client';
+import { useEffect, useState } from 'react';
+
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
-  );
+  const [message, setMessage] = useState('loading');
+  useEffect(() => {
+    fetch('http://localhost:8080/api/home')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setMessage(data.message);
+      });
+  }, []);
+
+  return <main className="">{message}</main>;
 }
