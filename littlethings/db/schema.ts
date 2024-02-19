5048710fb667f189da32b67ba62a952d0d0dfe3f
Brian Lee <bnlee419@gmail.com>
2024-02-19 00:18:00 -0800

    changed db/schema.ts for signup

--- littlethings/db/schema.ts
+++ littlethings/db/schema.ts
@@ -1,8 +1,7 @@
 import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core';
 
 export const users = sqliteTable('users', {
-  firstName: text('first_name'),
-  lastName: text('last_name'),
+  username: text('username'),
   email: text('email'),
   password: text('password'),
 });
--- littlethings/src/app/(auth)/login/layout.tsx
+++
@@ -1,9 +0,0 @@
-import Login from './page';
-
-export default function Layout({ children }): JSX {
-  return (
-    <>
-      <main>{children}</main>
-    </>
-  );
-}
